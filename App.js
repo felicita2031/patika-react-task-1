87f84a5faaba14216afe13504286b3b4d84d8f84
felicita2031 <79199431+felicita2031@users.noreply.github.com>
2022-07-09 20:18:35 +0300

    Update App.js

--- App.js
+++ App.js
@@ -4,7 +4,6 @@ import { getData } from "./getData";
 function App() {
     getData(1).then((res) => console.log(res.getUser.data, res.getPost.data));
 
-    return <div className="App"></div>;
-}
+    
 
-export default App;
+App();
